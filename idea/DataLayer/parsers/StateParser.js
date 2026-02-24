f49f91d164374402d831847792eaa27531e7c69f
Matthew <lojsamatvej@gmail.com>
2026-02-24 14:04:59 +0300

    Edited functionality of parsingStates method

--- idea/DataLayer/parsers/StateParser.js
+++ idea/DataLayer/parsers/StateParser.js
@@ -4,12 +4,14 @@ class StateParser {
     static parsingStates(jsonData){
         const statesArray = [];
         for(const code in jsonData){
+            const rawData = jsonData[code] // Added varaible
             let totalLat = 0;
             let totalLong = 0;
+            let totalResult = 0
             let pointsCount = 0;
 
-            polygons.forEach(poligon => {
-                poligon.forEach(sub => {
+            /*polygons.forEach(polygon => {
+                polygon.forEach(sub => {
                     if(typeof(sub[0]) ==='number'){
                         totalLong += sub[0];
                         totalLat += sub[1];
@@ -23,12 +25,21 @@ class StateParser {
                         })
                     }
                 })
-            });
+            });*/
+            const flatPoints = rawData.flat(Infinity)
+            for(let i = 0; i < flatPoints.length; i += 2){
+                totalLong += flatPoints[i];
+                totalLat += flatPoints[i+1];
+                //totalResult = totalLat + totalLong;
+            }
+
+            pointsCount = flatPoints.length / 2;
+
             const center ={
                 lat: totalLat / pointsCount,
                 long: totalLong / pointsCount
             }
-            const newState = new State(center);
+            const newState = new State(code, rawData, center); // Added cause in general we will have empty object
             statesArray.push(newState);
 
         }
