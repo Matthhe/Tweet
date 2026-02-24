8f0edb55e4057fe0efbb075663e6c911addc8908
Matthew <lojsamatvej@gmail.com>
2026-02-24 17:26:37 +0300

    Added new class TweetParser.js and have done functionality: 1) parsing lines by regex; 2) getting words from file by regex too

--- idea/DataLayer/parsers/TweetParser.js
+++ idea/DataLayer/parsers/TweetParser.js
@@ -1,19 +1,29 @@
 // фильтрация по термину, поиск ближайшего штата, группировка, усреднение.
 import Tweet from "../../BusinessLayer/models/Tweet.js";
-import fs from "fs";
-import path from "path";
-class TweetParser{
-    constructor(){
-        this.datePath = path.join(process.cwd(), 'idea', 'DataLayer', 'date');
+class TweetParser {
+    static parseLine(line){
+        const newLine = line.split("\t");
+        const regexNumbers = /-?\d+\.?\d*/g;
+
+        const resultNumbers = newLine[0].match(regexNumbers);
+        let lat = Number(resultNumbers[0]);
+        let long = Number(resultNumbers[1]);
+
+        let data = newLine[2];
+        let text = newLine[3];
+        return new Tweet(long, lat, data, text);
     }
 
-    async readTweets(){
+    static getWords(text){
+        const newText = text.toLowerCase();
+        const regexWords = /[a-z]+/g;
 
-        const filePath = fs.join(this.datePath, 'weekend_tweets2014.txt');
-
-        const content = await fs.readFile(filePath, 'utf8');
-
-
-
+        const resultWords = newText.match(regexWords);
+        if (resultWords === null) {
+            return [];
+        }
+        return resultWords;
     }
 }
+
+export default TweetParser;
