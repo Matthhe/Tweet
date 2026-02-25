0784116d65f6e01cde197dc95cd98063a6985130
Matthew <lojsamatvej@gmail.com>
2026-02-25 14:52:52 +0300

    Added and ended Sentiment Analyser

--- idea/BusinessLayer/services/SentimentAnalyser.js
+++ idea/BusinessLayer/services/SentimentAnalyser.js
@@ -1,1 +1,23 @@
-// Загрузка словаря текста и расчет сентимента
+import TweetParser  from "../../DataLayer/parsers/TweetParser.js";
+
+class SentimentAnalyser{
+    static sentimentAnalyser(tweet, results){
+        const words = TweetParser.getWords(tweet.text);
+
+        let totalScore = 0;
+        let foundWords = 0;
+
+        for( let word of words ){
+            if(results[word] !== undefined){
+                totalScore+= results[word];
+                foundWords++;
+            }
+        }
+        if(foundWords > 0){
+            return totalScore / foundWords;
+        }
+        else{
+            return null;
+        }
+    }
+}
