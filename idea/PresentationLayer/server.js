66ae96e0958b1841b774cce1f2cece7d25c74999
Matthew <lojsamatvej@gmail.com>
2026-03-03 15:47:51 +0300

    added back-end part of this programm

--- idea/PresentationLayer/server.js
+++ idea/PresentationLayer/server.js
@@ -1,1 +1,22 @@
-const express = require('express')
+import AnalysisManager from './AnalysisManager.js';
+import FileRepository from "../DataLayer/FileRepository.js";
+import express from 'express';
+import path from 'path';
+const app = express();
+const PORT = 3000
+
+app.use(express.static(path.join(process.cwd(), 'PresentationLayer', 'public')));
+
+app.get('/api/states', async (req, res) => {
+    const repo = new FileRepository();
+    const readState = await repo.readStates();
+    res.json(readState);
+})
+
+app.get('/api/trends/:filename', async (req, res) => {
+    const analyze = await AnalysisManager.analyze(req.params.filename);
+    res.json(analyze);
+})
+
+app.listen(PORT, () => console.log('Server is running on port ' + PORT));
+
