629d7b8cf549129862a0c6fb0c20be2495400561
Matthew <lojsamatvej@gmail.com>
2026-03-10 16:58:43 +0300

    implemented function which return color by analyzing sentiment

--- idea/PresentationLayer/public/app.js
+++ idea/PresentationLayer/public/app.js
@@ -13,3 +13,22 @@ function project(lon, lat){
     const y = height - (lat - minLat) * (height / (maxLat - minLat));
     return { x, y };
 }
+
+function getColor(sentiment){
+    if(sentiment === null){
+        return 'rgb(128, 128, 128)'
+    }
+    if(sentiment === 0) return 'rgb(255, 255, 255)'
+
+    let r, g, b;
+    if(sentiment > 0){
+        r = 255;
+        g = Math.round(sentiment * 255);
+        b = 0;
+    } else {
+        r = 0;
+        g = Math.round(Math.abs(sentiment) * 255);
+        b = 255;
+    }
+    return `rgb(${r}, ${g}, ${b})`;
+}
